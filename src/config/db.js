38700e4e3eb79c0cbba9597a93a86adedc04109a
Pratul <Pratuld@mindfiresolutions.com>
2026-03-11 15:06:10 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,34 +1,12 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./env");
 
-// module.exports = async () => {
-//   try {
-//     await mongoose.connect(MONGO_URI);
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.error("DB connection failed:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false,
-    });
+module.exports = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("DB connection failed:", err.message);
+    process.exit(1);
   }
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
-}
-
-module.exports = connectDB;
+};
