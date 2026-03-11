80b24375e3fed87b8a092d7785f4b22f4c6a1bbc
Pratul <Pratuld@mindfiresolutions.com>
2026-03-11 12:36:27 +0530

    added commega

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const connectDB = require("./src/config/db");
 const { PORT } = require("./src/config/env");
 
 (async function () {
+  console.log("Connecting to database...");
   await connectDB();
+  console.log("Database connected successfully");
   const app = require("./src/app");
   //   app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
 })();
