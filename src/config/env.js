b56b3cf9dd2825095e7eb5d0522365dfb0c180df
Pratul <Pratuld@mindfiresolutions.com>
2026-03-11 12:28:54 +0530

    update env

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,9 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   MONGO_URI: process.env.MONGO_URI,
   UPLOAD_DIR: process.env.UPLOAD_DIR || "uploads",
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+  CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRE: process.env.JWT_EXPIRE || "7d",
 };
