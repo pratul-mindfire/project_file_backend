2bc164e08e1fffbce307b91c83d917cf657e75f6
Pratul <Pratuld@mindfiresolutions.com>
2026-03-11 12:32:38 +0530

    add env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const connectDB = require("./src/config/db");
 const { PORT } = require("./src/config/env");
 
--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 module.exports = {
   PORT: process.env.PORT || 3000,
   MONGO_URI: process.env.MONGO_URI,
